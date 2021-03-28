a493816334eae243b589b71b8004e03c5b2b1d75
Umfy The Cat <tarabi98@gmail.com>
2021-03-28 23:28:12 +0200

    1st+

--- models/user.js
+++ models/user.js
@@ -3,23 +3,24 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-   name: { type: String, required: true, maxlength: 100 },
-   surname: { type: String, required: true, maxlength: 100 },
-   email: { type: String, maxlength: 100 },
-   spec: { type: String, maxlength: 100 },
-   shift: { type: Number },
-   isTech: { type: Boolean },
-   isAdmin: { type: Boolean },
+  name: { type: String, required: true, maxlength: 100 },
+  surname: { type: String, required: true, maxlength: 100 },
+  email: { type: String, maxlength: 100 },
+  phone: { type: String, maxlength: 100 },
+  spec: { type: String, maxlength: 100 },
+  shift: { type: Number },
+  isTech: { type: Boolean },
+  isAdmin: { type: Boolean },
 })
 
 // Virtual for user's full name
 UserSchema.virtual('fullname').get(function () {
-   return this.surname + ', ' + this.name
+  return this.surname + ', ' + this.name
 })
 
 // Virtual for user's URL
 UserSchema.virtual('url').get(function () {
-   return '/api/user/' + this._id
+  return '/api/user/' + this._id
 })
 
 //Export model
